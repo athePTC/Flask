689b3d1dbef85b1e96cc2db5aa93b926fa0ccd13
athePTC <athe@ptc.com>
2023-07-06 09:08:42 -0400

    FIx socket issues

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, send_file
 from apiTest import download_svg_from_image
 import subprocess as sp
 import os
+import socket
 
 app = Flask(__name__)
 
@@ -17,6 +18,23 @@ def home():
             return send_file(os.path.join(output_folder, filename + '.fbx'), as_attachment=True)
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+def get_open_port():
+    """
+    Find an available port by attempting to bind to a socket with port 0.
+    Returns the port number if successful, or None if no port is available.
+    """
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind(('localhost', 0))
+        _, port = sock.getsockname()
+        sock.close()
+        return port
+    except OSError:
+        return None
 
+if __name__ == '__main__':
+    port = get_open_port()
+    if port is None:
+        print("No available port found.")
+    else:
+        app.run(host='0.0.0.0', port=port)
